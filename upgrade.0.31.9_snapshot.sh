b1f019a31b039572383ffdf27a150241d9f746ad
StakeandRelax Validator <107054983+StakeandRelax-Validator@users.noreply.github.com>
2024-03-08 08:20:50 +0100

    Create upgrade.0.31.9_snapshot.sh

---
+++ upgrade.0.31.9_snapshot.sh
@@ -0,0 +1,19 @@
+NAMADA_TAG="v0.31.9"
+cd namada
+git checkout $NAMADA_TAG
+make build-release
+sudo mv target/release/namada /usr/local/bin/
+sudo mv target/release/namada[c,n,w] /usr/local/bin/
+
+cd $HOME
+wget https://namada-se-rpc.citadel.one/snap/namada-se-90000-tx_index.tar.lz4
+mkdir temp
+lz4 -d namada-se-90000.tar.lz4 -c | tar xvf - -C ./temp/
+
+rm -rf .local/share/namada/shielded-expedition.88f17d1d14/db
+mv temp/db .local/share/namada/shielded-expedition.88f17d1d14/
+
+mv .local/share/namada/shielded-expedition.88f17d1d14/cometbft/data/priv_validator_state.json .local/share/namada/shielded-expedition.88f17d1d14/cometbft/
+rm -rf .local/share/namada/shielded-expedition.88f17d1d14/cometbft/data/
+mv temp/data/ .local/share/namada/shielded-expedition.88f17d1d14/cometbft/
+mv .local/share/namada/shielded-expedition.88f17d1d14/cometbft/priv_validator_state.json .local/share/namada/shielded-expedition.88f17d1d14/cometbft/data/
